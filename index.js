5074b5b21a0459a1ecb53c54c96b28330f7e9a35
Hatem Azaiez <hatemazaiez1@gmail.com>
2025-09-13 18:28:37 +0100

    cors *

--- index.js
+++ index.js
@@ -1089,15 +1089,10 @@ const app = express();
 
 // Configure CORS to allow dashboard access
 app.use(cors({
-    origin: [
-        'http://localhost:3000',
-        'http://localhost:3001',
-        'http://127.0.0.1:3000',
-        'http://127.0.0.1:3001'
-    ],
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
+    credentials: false
 }));
 
 app.use(express.json());
